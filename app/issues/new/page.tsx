caa0de74d9b67c85cd8ae97425be6c06474c565e
mgundu <mamatha22skca@gmail.com>
2025-03-05 15:15:31 -0500

    Extract the error message component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Button, TextField, Callout, Text } from '@radix-ui/themes'
+import { Button, TextField, Callout } from '@radix-ui/themes'
 import React from 'react'
 import SimpleMDE from "react-simplemde-editor";
 import { useForm , Controller} from 'react-hook-form';
@@ -10,6 +10,7 @@ import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchema';
 import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -37,12 +38,17 @@ type IssueForm = z.infer<typeof createIssueSchema>;
          <TextField.Root placeholder='Title' {...register('title')}>
            <TextField.Slot />
          </TextField.Root>
-         {errors.title && <Text color="red">{errors.title.message}</Text>}
+         {errors.title && (
+          <ErrorMessage>
+            {errors.title?.message}
+          </ErrorMessage>)}
          <Controller name="description" 
          control={control} 
          render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
          />
-         {errors.description && <Text color="red">{errors.description.message}</Text> }
+         <ErrorMessage>
+            {errors.description?.message}
+          </ErrorMessage>
          <Button> Submit New Issue </Button>
        </form>
        </div>
